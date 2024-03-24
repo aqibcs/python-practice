624580465047889c3c43583ede87b537e41cd45d
aqibcs <aqibshahcs@gmail.com>
2024-03-24 14:24:46 +0500

    Immutable object in python

---
+++ immutables/immutable.py
@@ -0,0 +1,10 @@
+# Tuple Example
+python_tuple = ('data', 'science')
+python_tuple[1] = 'object'
+print(python_tuple)
+
+
+# Integer Example
+python_integer = 1234
+python_integer[2] = 5
+print(python_integer)

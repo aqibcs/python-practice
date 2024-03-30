8e21b9992b8935c67c9082272b3e9946ceccb600
aqibcs <aqibshahcs@gmail.com>
2024-03-30 20:36:08 +0500

    multi assignment

---
+++ multi-assignment/main.py
@@ -0,0 +1,23 @@
+# Multiple Assignment = allow us to assign multiple variables at the same time in one code
+
+# name = "Jhon"
+# age = 21
+# attractive = True
+
+name, age, attractive = "Jhon", 21, True
+
+print(name)
+print(age)
+print(attractive)
+
+# Spongebob = 20
+# Patrick = 20
+# Sandy = 20
+# Squidward = 20
+
+Spongebob = Patrick = Sandy = Squidward = 20
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)

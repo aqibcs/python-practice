ba2744617299522116a9160bf772c058d68e3243
aqibcs <aqibshahcs@gmail.com>
2024-03-31 21:15:15 +0500

    Type Casting

---
+++ type-cast/main.py
@@ -0,0 +1,13 @@
+# Type Casting = convert the data type of a value to another data type
+
+x = 1    #int
+y = 2.0 #float
+z = "3"  #str
+
+x = float(x)
+y = int(y)
+z = str(z)
+
+print("X is: "+str(x))
+print("Y is: "+str(y))
+print("Z is: "+z*3)

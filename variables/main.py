a05f1c5042214c3acdeb8d307f4ef9a85128397d
aqibcs <aqibshahcs@gmail.com>
2024-04-01 19:57:07 +0500

    Variables

---
+++ variables/main.py
@@ -0,0 +1,20 @@
+# Variables
+first_name = "Jhon"
+last_name = "Doe"
+full_name = first_name  +" "+  last_name
+print("hello  "+ full_name)
+
+
+age = 21
+age += 1
+print("Your age is: "+str(age))
+print(type(age))
+
+
+height = 250.5
+print(type(height))
+print("Your height is: "+str(height)+"cm")
+
+human = True
+print(type(human))
+print("Are you human: "+str(human))

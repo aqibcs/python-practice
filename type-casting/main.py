fa488baced45ef0f07fc21f6d800623aac935d11
aqibcs <aqibshahcs@gmail.com>
2024-04-22 21:13:13 +0500

    Type Casting in Python

---
+++ type-casting/main.py
@@ -0,0 +1,54 @@
+# Python automatically converts
+# a to int
+a = 7
+print(type(a))
+
+# Python automatically converts
+# b to float
+b = 4.0
+print(type(b))
+
+# Python automatically converts 
+# c to float as it is a float addition 
+c = a + b
+print(c)
+print(type(c))
+
+# Python automatically converts 
+# d to float as it is a float multiplication
+d = a * b
+print(d)
+print(type(d))
+
+# Here, we are Converting Int to Float in Python with the float() function.
+# int variable
+e = 5
+
+# typecast to float
+n = float(e)
+print(n)
+print(type(n))
+
+# float variable
+e = 5.9
+
+# typecast to int
+n = int(e)
+print(n)
+print(type(n))
+
+# int variable
+f = 5
+
+# typecast to str
+n = str(a)
+print(n)
+print(type(n))
+
+# string variable
+g = "5.0"
+
+# typecast to int
+n = float(g)
+print(n)
+print(type(n))
